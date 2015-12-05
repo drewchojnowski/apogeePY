faac224c3f8ce91286fb0849aedfa78a3c116a27
drewchojnowski <drewski@nmsu.edu>
2015-12-04 18:33:30 -0700

    updates

--- apogeespec.py
+++ apogeespec.py
@@ -1,4 +1,15 @@
+'''
+---------------------------------------------------------------------
+APOGEESPEC
+
+Basic tools for dealing with APOGEE spectra of emission line stars.
+
+Written by Drew Chojnowski
+---------------------------------------------------------------------
+'''
+
 import math
+import glob
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
@@ -6,6 +17,13 @@ from astropy.io import fits
 from astropy.io import ascii
 from scipy.interpolate import interp1d
 
+specdir='../spectra/'
+linefile='linelist.dat'
+starcatalog='catalog.dat'
+
+'''
+APLOAD: LOAD THE APVISIT WAVE,FLUX INTO A SINGLE ARRAY
+'''
 def apload(file):
     # read the FITS file
     hdulist=fits.open(file)
@@ -20,15 +38,122 @@ def apload(file):
 
     return allwave,allflux
 
-def splot(file,linefile='linelist.dat',mark_sky=False,mark_lines=True,
-    xshift=0.0,do_cont=True,do_cont_chips=False,cont_ord=5):
+'''
+APCLOAD_CHIPS: LOAD THE APVISIT WAVE,FLUX INTO INDIVIDUAL ARRAYS
+'''
+def apload_chips(file):
+    # read the FITS file
+    hdulist=fits.open(file)
+    wave=hdulist[4].data
+    flux=hdulist[1].data
 
-    # read the linelist
+    # get single arrays of wavelength and flux values, ordered properly
+    rw=np.array(wave[0]); rw=rw[::-1]; rw=rw.tolist(); rf=flux[0]; rf=rf[::-1]; rf=rf.tolist()
+    gw=np.array(wave[1]); gw=gw[::-1]; gw=gw.tolist(); gf=flux[1]; gf=gf[::-1]; gf=gf.tolist()
+    bw=np.array(wave[2]); bw=bw[::-1]; bw=bw.tolist(); bf=flux[2]; bf=bf[::-1]; bf=bf.tolist()
+    allwave=np.array(bw+gw+rw); allflux=np.array(bf+gf+rf)
+
+    return rw,rf
+
+'''
+BROWSER: INTERACTIVE RA/DEC PLOT
+'''
+def browser():
+    startable=ascii.read(starcatalog)
+    abeID=np.array(startable['ID'])
+    tmassID=np.array(startable['2MASS'])
+
+    spectra=np.array(glob.glob(specdir+'*apV*fits'))
+    nspec=len(spectra)
+    stars_all=[]
+    ra_all=np.zeros(nspec); dec_all=np.zeros(nspec)
+    glon_all=np.zeros(nspec); glat_all=np.zeros(nspec)
+    snr_all=np.zeros(nspec); hmag_all=np.zeros(nspec)
+    for i in range(nspec):
+        hdulist=fits.open(spectra[i])
+        stars_all.append(hdulist[0].header['objid'])
+        ra_all[i]=hdulist[0].header['ra']
+        dec_all[i]=hdulist[0].header['dec']
+        glon_all[i]=hdulist[0].header['glon']
+        glat_all[i]=hdulist[0].header['glat']
+        snr_all[i]=hdulist[0].header['snr']
+        hmag_all[i]=hdulist[0].header['h']
+
+    stars_all=np.array(stars_all)
+    stars,indices=np.unique(stars_all,return_index=True)
+    nstars=len(stars)
+    ra=ra_all[indices]
+    dec=dec_all[indices]
+    glon=glon_all[indices]
+    glat=glat_all[indices]
+    hmag=hmag_all[indices]
+
+    fig, ax = plt.subplots(figsize=(12,7))
+    ax.set_title('click on point to show highest S/N spectrum')
+    line,=ax.plot(ra,dec,'o',picker=5,markersize=12)  # 5 points tolerance
+    for i in range(nstars):
+        p=np.where(stars[i]==tmassID)
+        ax.text(ra[i]+3,dec[i],abeID[p][0],fontsize=10)
+
+    plt.xlabel('RA')
+    plt.ylabel('DEC')
+    plt.tight_layout() 
+
+    def onpick(event):
+        if event.artist != line:
+            return True
+
+        N=len(event.ind)
+        if not N:
+            return True
+
+        for subplotnum, dataind in enumerate(event.ind):
+            star=stars[dataind]
+            p=np.where(star==stars_all)
+            gd=np.where(snr_all[p]==np.max(snr_all[p]))
+            specplot=apsplot(spectra[p][gd][0],mark_sky=True)
+        return True
+
+    fig.canvas.mpl_connect('pick_event', onpick)
+
+    return
+
+'''
+APSPLOT_ON_KEY: KEY PRESS EVENT HANDLER FOR APSPLOT
+'''
+def apsplot_on_key(event):
     linelist=ascii.read(linefile)
+    global key, xdata, ydata
+    key=event.key
+    xdata=event.xdata; ydata=event.ydata
+    xdata_str="%0.3f" % xdata
+    ydata_str="%0.3f" % ydata
 
-    # get the wavelength and flux arrays
-    wave,flux=apload(file)
-    wave=wave+xshift
+     # find the nearest line in the linelist
+    dist=abs(xdata-linelist['CENT'])
+    x=np.where(dist==min(dist))
+    z=x[0].astype('int')
+    rest=linelist['CENT'][z]
+    rest_str="%0.3f" % rest[0]
+    dif=rest[0]-xdata
+    dif_str="%0.3f" % dif
+    closeline=linelist['LABEL'][z][0].tolist()
+    if abs(dif) > 8:
+        print(key)
+        print(xdata_str+r'....No lines within 10 $\AA$')
+    else:
+        print(key)
+        bla=xdata_str+'....nearest line= '+closeline+' '+rest_str+', dif= '+dif_str+')'
+        print(bla)
+
+'''
+APSPLOT: SPECTRUM PLOTTING PROGRAM
+'''
+def apsplot(file,mark_sky=False,mark_lines=True,xshift=0.0,do_cont=True,do_cont_chips=False,cont_ord=5):
+    linelist=ascii.read(linefile)    # read the linelist
+
+    wave,flux=apload(file)    # get the wavelength and flux arrays
+    wave=wave+xshift    # option to add on an xshift, in angstroms
 
     # get some header values from the FITS file
     hdulist=fits.open(file)
@@ -71,11 +196,16 @@ def splot(file,linefile='linelist.dat',mark_sky=False,mark_lines=True,
             flux=flux/cont
 
     # make the plot
-    fig=plt.figure(figsize=(18,9))
-    matplotlib.rcParams.update({'font.size': 16, 'font.family':'serif'})
+    fig=plt.figure(figsize=(16,8))
+    matplotlib.rcParams.update({'font.size': 14, 'font.family':'serif'})
     ax=fig.add_subplot(111)
-    plt.plot(wave,flux,color='black')
+    ax.plot(wave[np.where((wave>15144)&(wave<15808))],flux[np.where((wave>15144)&(wave<15808))],color='black')
+    ax.plot(wave[np.where((wave>15858)&(wave<16432))],flux[np.where((wave>15858)&(wave<16432))],color='black')
+    ax.plot(wave[np.where((wave>16473)&(wave<16955))],flux[np.where((wave>16473)&(wave<16955))],color='black')
+#    ax.plot(wave,flux,color='black')
+    ax.grid(True)
     plt.xlim([15135,16965])
+    plt.ylim([0.5,1.5])
     plt.xlabel(r'Observed Wavelength [$\AA$]')
     plt.ylabel(r'Flux [10$^{-17}$ erg s$^{-1}$ cm$^{-2}$ $\AA$]')
     plt.title(file+'    '+objid+'    S/N='+snr+'    exptime='+exptime+' s')
@@ -115,34 +245,9 @@ def splot(file,linefile='linelist.dat',mark_sky=False,mark_lines=True,
                 ax.arrow(line,arrowstart,0,arrowlen,head_width=3,head_length=arrowheadL,color=labcol)
                 ax.text(line,txty,lab.replace("_"," "),rotation=90,ha='center',va='bottom',fontsize=9,color=labcol)
 
-    # define a key press event
-    def on_key(event):
-        linelist=ascii.read(linefile)
-        global key, xdata, ydata
-        key=event.key
-        xdata=event.xdata
-        ydata=event.ydata
-        xdata_str="%0.3f" % xdata
-        ydata_str="%0.3f" % ydata
-
-        # find the nearest line in the linelist
-        dist=abs(xdata-linelist['CENT'])
-        x=np.where(dist==min(dist))
-        z=x[0].astype('int')
-        rest=linelist['CENT'][z]
-        rest_str="%0.3f" % rest[0]
-        dif=rest[0]-xdata
-        dif_str="%0.3f" % dif
-        closeline=linelist['LABEL'][z][0].tolist()
-
-        if abs(dif) > 8:
-            print(xdata_str+r'....No lines within 10 $\AA$')
-        else:
-            bla=xdata_str+'....nearest line= '+closeline+' '+rest_str+', dif= '+dif_str+')'
-            print(bla)
-
-    cid=fig.canvas.mpl_connect('key_press_event',on_key)
+    cid=fig.canvas.mpl_connect('key_press_event',apsplot_on_key)
 
     hdulist.close()
+    plt.show()
 
     return
